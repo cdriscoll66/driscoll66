5bf7baab5a5175e7accc6fd83f11e0522ae4e878
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-04-21 16:43:46 -0400

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <h1>Conor's Home</h1>
+      <p>
+        we are about to embark on a plan to put together a strategy that will
+        begin to form into a thing....
+      </p>
+      <p>Stay tuned</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
